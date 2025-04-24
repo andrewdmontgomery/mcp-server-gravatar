03f2388bbf44c13b0a6b5a5cbb1ced737e7a8da5
Andrew Montgomery <andrewdmontgomery@mac.com>
2025-04-23 20:57:22 -0500

    Add more resources

--- src/mcp_server_gravatar/resources.py
+++ src/mcp_server_gravatar/resources.py
@@ -5,7 +5,7 @@ from .tools import profile_tools, avatar_tools
 
 def register_resources(mcp: FastMCP):
     @mcp.resource(
-        uri="gravatar://profiles/{profileIdentifier}",
+        uri="profiles://profileIdentifier/{profileIdentifier}",
         name="Get Profile by ID",
         description="Returns a profile object as json",
         mime_type="application/json")
@@ -14,10 +14,28 @@ def register_resources(mcp: FastMCP):
         return json.dumps(profile)
 
     @mcp.resource(
-        uri="gravatar://me/avatars",
+        uri="profiles://email/{email}",
+        name="Get Profile by email",
+        description="Returns a profile object as json",
+        mime_type="application/json")
+    async def get_profile(email: str) -> str:
+        profile = await profile_tools._get_profile_by_email(email)
+        return json.dumps(profile)
+
+    @mcp.resource(
+        uri="avatars://me",
         name="List Avatars",
         description="Returns a list of avatars",
         mime_type="application/json")
     async def get_avatars() -> str:
         avatars = await avatar_tools._get_avatars()
+        return json.dumps(avatars)
+
+    @mcp.resource(
+        uri="avatars://me/{selected_email_hash}",
+        name="List Avatars showing selected",
+        description="Returns a list of avatars",
+        mime_type="application/json")
+    async def get_avatars(selected_email_hash: str) -> str:
+        avatars = await avatar_tools._get_avatars(selected_email_hash=selected_email_hash)
         return json.dumps(avatars)
