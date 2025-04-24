9e14e9c6f877d78963078d553aa65588230e72ae
Andrew Montgomery <andrewdmontgomery@mac.com>
2025-04-23 21:10:24 -0500

    Remove the avatar resources, since the underlying API call is currently broken

--- src/mcp_server_gravatar/resources.py
+++ src/mcp_server_gravatar/resources.py
@@ -21,21 +21,3 @@ def register_resources(mcp: FastMCP):
     async def get_profile(email: str) -> str:
         profile = await profile_tools._get_profile_by_email(email)
         return json.dumps(profile)
-
-    @mcp.resource(
-        uri="avatars://me",
-        name="List Avatars",
-        description="Returns a list of avatars",
-        mime_type="application/json")
-    async def get_avatars() -> str:
-        avatars = await avatar_tools._get_avatars()
-        return json.dumps(avatars)
-
-    @mcp.resource(
-        uri="avatars://me/{selected_email_hash}",
-        name="List Avatars showing selected",
-        description="Returns a list of avatars",
-        mime_type="application/json")
-    async def get_avatars(selected_email_hash: str) -> str:
-        avatars = await avatar_tools._get_avatars(selected_email_hash=selected_email_hash)
-        return json.dumps(avatars)
